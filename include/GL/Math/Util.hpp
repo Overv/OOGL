dba8f34c45109dea1e7b876961a442d63584b046
Alexander Overvoorde <overv161@gmail.com>
2012-06-24 17:04:50 +0200

    Added degrees/radians utility functions.

---
+++ include/GL/Math/Util.hpp
@@ -0,0 +1,42 @@
+/*
+	Copyright (C) 2012 Alexander Overvoorde
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of
+	this software and associated documentation files (the "Software"), to deal in
+	the Software without restriction, including without limitation the rights to
+	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+	the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+*/
+
+#pragma once
+
+#ifndef OOGL_UTIL_HPP
+#define OOGL_UTIL_HPP
+
+namespace GL
+{
+	const float PI = 3.14159265358979323846f;
+
+	inline float Rad( float degrees )
+	{
+		return degrees / 180.0f * PI;
+	}
+
+	inline float Deg( float radians )
+	{
+		return radians / PI * 180.0f;
+	}
+}
+
+#endif
