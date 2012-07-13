0a812168b9abe0625650be75ac8d2633dcd97229
Alexander Overvoorde <overv161@gmail.com>
2012-07-14 00:23:08 +0200

    Finished JPEG support.

--- src/GL/Util/Image.cpp
+++ src/GL/Util/Image.cpp
@@ -463,6 +463,54 @@ namespace GL
 
 	void Image::SaveJPEG( const std::string& filename )
 	{
+		FILE* file = fopen( filename.c_str(), "wb" );
+		if ( !file ) throw FileException();
+
+		// Initialize structures
+		jpeg_compress_struct cinfo;
+		jpeg_error_mgr jerr;
+
+		cinfo.err = jpeg_std_error( &jerr );
+		jpeg_create_compress( &cinfo );
 		
+		cinfo.image_width = width;
+		cinfo.image_height = height;
+		cinfo.input_components = 3;
+		cinfo.in_color_space = JCS_RGB;
+
+		// Configure image
+		jpeg_stdio_dest( &cinfo, file );
+		jpeg_set_defaults( &cinfo );
+		jpeg_set_quality( &cinfo, 90, true );
+
+		// Prepare pixel data
+		std::vector<uchar> pixelData;
+		pixelData.reserve( width * height * 3 );
+
+		for ( ushort y = 0; y < height; y++ )
+		{
+			for ( ushort x = 0; x < width; x++ )
+			{
+				Color& col = image[ x + y * width ];
+				pixelData.push_back( col.R );
+				pixelData.push_back( col.G );
+				pixelData.push_back( col.B );
+			}
+		}
+
+		// Compress
+		jpeg_start_compress( &cinfo, true );
+
+		for ( ushort y = 0; y < height; y++ )
+		{
+			JSAMPROW row = &pixelData[ y * width * 3 ];
+			jpeg_write_scanlines( &cinfo, &row, 1 );
+		}
+
+		jpeg_finish_compress( &cinfo );
+
+		jpeg_destroy_compress( &cinfo );
+
+		fclose( file );
 	}
 }
