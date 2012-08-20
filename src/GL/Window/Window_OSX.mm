e1f33a5382e0fdc376afc7abde260905fe3e6922
Jack Karamanian <karamanian.jack@gmail.com>
2012-08-20 14:11:36 -0500

    Changed copyright notice.

--- src/GL/Window/Window_OSX.mm
+++ src/GL/Window/Window_OSX.mm
@@ -1,10 +1,23 @@
-//
-//  Window_OSX.mm
-//  OOGL
-//
-//  Created by Jack Karamanian on 8/4/12.
-//  Copyright (c) 2012 Jack Karamanian. All rights reserved.
-//
+/*
+     Copyright (C) 2012 Jack Karamanian
+     
+     Permission is hereby granted, free of charge, to any person obtaining a copy of
+     this software and associated documentation files (the "Software"), to deal in
+     the Software without restriction, including without limitation the rights to
+     use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+     the Software, and to permit persons to whom the Software is furnished to do so,
+     subject to the following conditions:
+     
+     The above copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+     
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+     FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+     COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+     IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+ */
 #include <GL/Window/Window.hpp>
 
 #ifdef OOGL_PLATFORM_OSX
@@ -131,6 +144,7 @@ namespace GL {
             [NSApp updateWindows];
         } while (event);
         
+        // From Window_X11.cpp
 		// Return oldest event - if available
 		if ( events.empty() ) return false;
 		
