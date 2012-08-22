cc52d6d85f29f0262ce44c18a990fe4e610264c0
Jack Karamanian <karamanian.jack@gmail.com>
2012-08-21 21:00:57 -0500

    Added targets in the Xcode project for each sample.

---
+++ build/xcode/OOGL Samples/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OOGL Samples
+//
+//  Created by Jack Karamanian on 8/21/12.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
